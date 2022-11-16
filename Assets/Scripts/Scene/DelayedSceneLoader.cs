f612b1470847a3b48213ecdf59238db1a0e3373c
Rachit Raj <rachitraj2904@gmail.com>
2022-11-16 20:03:14 +0530

    load scene after a delay to show loading screen

---
+++ Assets/Scripts/Scene/DelayedSceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+namespace Scene
+{
+    public class DelayedSceneLoader : MonoBehaviour
+    {
+        [SerializeField] private float loadDelay;
+        [SerializeField] private string sceneName;
+
+        private void Awake() => DelayedRunner.Instance.RunWithDelay(loadDelay, () => SceneManager.LoadScene(sceneName));
+    }
+}
---
+++ Assets/Scripts/Utils/DelayedRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Utils
+{
+    public class DelayedRunner : MonoBehaviour
+    {
+        private static DelayedRunner _instance;
+
+        public static DelayedRunner Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                var obj = new GameObject("DelayedRunnerInstance");
+                var runner = obj.AddComponent<DelayedRunner>();
+                _instance = runner;
+                return _instance;
+            }
+            private set => _instance = value;
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this) Instance = null;
+        }
+
+        public Coroutine RunWithDelay(float time, Action ac) => StartCoroutine(RunWithDelayCoroutine(time, ac));
+
+        private IEnumerator RunWithDelayCoroutine(float time, Action ac)
+        {
+            yield return new WaitForSeconds(time);
+            ac.Invoke();
+        }
+    }
+}
